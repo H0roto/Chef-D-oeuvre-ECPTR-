5a58b7db2567671124aa24021460c648061e1f10
Emile Thomas <thomas.emile@univ-tlse3.fr>
2026-03-02 11:12:38 +0100

    refactor : mise a jour entre repo disant et local

--- Thomas.sh
+++ Thomas.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#SBATCH --job-name=Test_git_SVD
+#SBATCH --job-name=Titre
 #SBATCH --output=Open-3DULM-main/Result/ML-%j-open_3D_ulm_main_Thomas.out
 #SBATCH --error=Open-3DULM-main/Result/ML-%j-open_3D_ulm_main_Thomas.err
 
@@ -9,7 +9,7 @@
 #SBATCH --mail-user=thomas.emile@univ-tlse3.fr
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --partition=GPUNodes
+#SBATCH --partition=RTX8000Nodes
 #SBATCH --gres=gpu:1
 #SBATCH --gres-flags=enforce-binding
 
